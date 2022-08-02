bf7435a5e23ab7522b5f33fe34206ece21729f0a
Ella Mäkelä <ellamakela@Ella-MBP.home>
2022-08-02 15:13:18 +0300

    vaihtoehtojen säätöi

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import otsikko from './data/otsikko.png';
 function App() {
   return (
     <div className="App">
-      <img src={otsikko} alt="Logo" className='otsikko' />
+      <div className='otsikko-wrapper'><img src={otsikko} alt="Logo" className='otsikko' /></div>
       <h1>Tylypuhkun lajitteluhattu!</h1>
-      <p>Helmikuisen lastenkonsertin jälkeen pidetyillä Puhkujen lastenkutsuilla järjestettiin salaisin menoin ensimmäinen lajittelu (1 op), jossa osallistujat jaettiin viime Puhkusian numerossa esiteltyihin Tylypuhkun tupiin: Röyhkelikkö, Puhkupöh, Kyrwänkynsi ja Huiluinen.</p>
-      <p>Haluaisitko silti tietää, tuletko olemaan myöhempien aikojen pivotulimestari tai ehkä peräti Poika Joka Kiksaa? Jos lajittelu jäi välistä, ei (paljon) hätää! Kaljaministeriön salainen Alavireisyyksien osasto on jäänyt keskiviikkoiltana ylitöihin ja laatinut testin, jolla jokainen voi testata, mihin tupaan kuuluu.</p>
+      <p>Helmikuussa 2014 lastenkonsertin jälkeen pidetyillä Puhkujen lastenkutsuilla järjestettiin salaisin menoin ensimmäinen lajittelu (1op), jossa osallistujat jaettiin Puhkusiassa esiteltyihin Tylypuhkun tupiin: Röyhkelikkö, Puhkupöh, Kyrwänkynsi ja Huiluinen.</p>
+      <p>Haluatko tietää, tuletko olemaan myöhempien aikojen pivotulimestari tai ehkä peräti Poika Joka Kiksaa? Kaljaministeriön salainen Alavireisyyksien osasto on laatinut testin, jolla jokainen voi testata, mihin tupaan kuuluu.</p>
       <LajitteluQuiz className="quiz" />
     </div>
   );
